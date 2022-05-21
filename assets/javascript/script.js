196e4762a3c20fafafb57ceb004a3f6c37dc89fc
ydennekrf <ydennekrf@gmail.com>
2022-05-21 18:20:24 -0400

    added event listener to answer buttons and check answer code

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,14 +1,12 @@
  let highscore = document.querySelector("#highscore");
  let question = document.querySelector("#question");
  let questionContainer = document.querySelector(".questioncontainer");
+ let answerContainer = document.querySelector(".answercontainer");
+ let answer1 = document.querySelector(".answers");
 
-//  let answer1 = document.querySelector("#answer1");
-//  let answer2 = document.querySelector("#answer2");
-//  let answer3 = document.querySelector("#answer3");
-//  let answer4 = document.querySelector("#answer4");
- let answerEl = document.querySelector(".answer");
- let startBtn = document.querySelector("#start");
+  let startBtn = document.querySelector("#start");
  let timerEl = document.querySelector("#timer");
+ let quizOver = document.querySelector(".quiz-over");
 
  //correct answer counter
  let score = 0;
@@ -16,17 +14,28 @@
  let isFinish = false;
  let timer;
  let timeLeft;
+ let questionIndex = 0;
 
+let questions = [    
+    {question: question.textContent = "this is question1",
+    answers: ["answer1", "answer2", "answer3", "answer4"],
+    correctIndex: "2"},
+    {question: question.textContent = "this is question2",
+    answers: ["ANSWER1", "ANSWER2", "ANSWER3", "ANSWER4"],
+    correctIndex: "3"},
+    {question: question.textContent = "this is question3",
+    answers: ["ANSWER1", "ANSWER2", "ANSWER3", "ANSWER4"],
+    correctIndex: "1"} 
+];
 
-//  let scoreArray = [(initials,finalScore)]
 
 init = () => {
     getScore();
-}
+};
 
 startGame = () => {
     isWin = false;
-    timeLeft = 90;
+    timeLeft = 10;
     //disables start button to be hit during game play
     startBtn.disabled = true;
     //switches the question area and timer box from hidden to visable
@@ -34,7 +43,7 @@ startGame = () => {
     questionContainer.setAttribute("style", "display: block;");
     // starts timer counting
     startTimer();
-    startQuestions();
+    // startQuestions();
 };
 
 startTimer = () => {
@@ -54,41 +63,37 @@ startTimer = () => {
             //stops timer and game over
             clearInterval(timer);
             // postQuiz();
-            // gameOver();
+            gameOver();
         }
     }, 1000);
 };
 
+gameOver = () => {
+    timerEl.setAttribute("style", "display: none;");
+    questionContainer.setAttribute("style", "display: none;");
+    quizOver.setAttribute("style", "display: block;");
+    quizOver.textContent = "OUT OF TIME";
+}
 
-
-
-
-
-// printAnswers = () => {
-//     let answerPrint = [answer1,answer2,answer3,answer4];
-   
-//     for (i = 0; i< answerPrint.length;i++) {
-//         answer = document.textContent.answerPrint[i];
-//     }
-//    };
-
-// gameOver = () => {
-//     //todo make game board dissappear and say Game Over form to "enter initials" push obj to highscore sort
-//     scoreCount();
-// } 
-
-// scoreCount = () => {
-//     if (questionPool === 0) {
-//         finalscore = score + timeLeft;
-//     } 
-// }
-
-// let timeInterval = setInterval(function() {
-//     timeLeft--;
-//     timer.textContent = timeLeft;
-
-    
-//   }, 1000);
+checkAnswer = (answerIndex) => {
+    if (questions[0].correctIndex === answerIndex){
+        console.log("correct")
+        questionIndex++
+        //next question, add score, add time
+    } else {
+        console.log("fail")
+        questionIndex++
+        //next question, minus time
+    }
+}
 
   //attach event listeners to start button, and answer buttons
   startBtn.addEventListener("click", startGame);
+  answerContainer.addEventListener("click", function(event){
+      let element = event.target;
+
+      if (element.matches(".answers")){
+          let answerIndex = element.dataset.index
+          checkAnswer(answerIndex);
+      }
+  })
