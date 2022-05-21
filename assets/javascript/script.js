6a27750f8edecae84b50679d73d88879b42fa9c5
ydennekrf <ydennekrf@gmail.com>
2022-05-21 14:06:03 -0400

    added code to start button that shows game area

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,60 +1,74 @@
  let highscore = document.querySelector("#highscore");
  let question = document.querySelector("#question");
+ let questionContainer = document.querySelector(".questioncontainer");
+
 //  let answer1 = document.querySelector("#answer1");
 //  let answer2 = document.querySelector("#answer2");
 //  let answer3 = document.querySelector("#answer3");
 //  let answer4 = document.querySelector("#answer4");
- let answer = document.querySelector(".answer");
+ let answerEl = document.querySelector(".answer");
  let startBtn = document.querySelector("#start");
- let timer = document.querySelector("#timer");
+ let timerEl = document.querySelector("#timer");
 
  //correct answer counter
  let score = 0;
  let finalScore = 0;
+ let isWin = false;
+ let timer;
+ let timeLeft;
 
- let scoreArray = [(initials,finalScore)]
 
-printAnswers = () => {
- let answerPrint = [answer1,answer2,answer3,answer4];
+//  let scoreArray = [(initials,finalScore)]
 
- for (i = 0; i< answerPrint.length;i++) {
-     answer = document.textContent.answerPrint[i];
- }
+init = () => {
+    getScore();
+}
+
+startGame = () => {
+    isWin = false;
+    timeLeft = 90;
+    //disables start button to be hit during game play
+    startBtn.disabled = true;
+    //switches the question area and timer box from hidden to visable
+    timerEl.setAttribute("style", "display: block;");
+    questionContainer.setAttribute("style", "display: block;");
+    // starts timer counting
+    // startTimer();
+    // startQuestions();
 };
 
-countdown = () => {
-let timeLeft = 90;
 
-let timeInterval = setInterval(function() {
-    timeLeft--;
-    timer.textContent = timeLeft;
 
-    if (timeLeft === 0) {
-        clearInterval(timeInterval);
-        gameOver();
-        alert("game over")
 
-    } else if (userAnswer == false) {
-      timeLeft = timeLeft - 10;
-      alert("wrong");
 
-    } else {
-        timeLeft = timeLeft + 10;
-        score++;
-        alert("correct!");
-    }
+
+
+
+// printAnswers = () => {
+//     let answerPrint = [answer1,answer2,answer3,answer4];
+   
+//     for (i = 0; i< answerPrint.length;i++) {
+//         answer = document.textContent.answerPrint[i];
+//     }
+//    };
+
+// gameOver = () => {
+//     //todo make game board dissappear and say Game Over form to "enter initials" push obj to highscore sort
+//     scoreCount();
+// } 
+
+// scoreCount = () => {
+//     if (questionPool === 0) {
+//         finalscore = score + timeLeft;
+//     } 
+// }
+
+// let timeInterval = setInterval(function() {
+//     timeLeft--;
+//     timer.textContent = timeLeft;
+
     
-  }, 1000);
-};
+//   }, 1000);
 
-gameOver = () => {
-    //todo make game board dissappear and say Game Over form to "enter initials" push obj to highscore sort
-    scoreCount();
-}
-
-scoreCount = () => {
-    if (questionPool === 0) {
-        finalscore = score + timeLeft;
-    } 
-}
-
+  //attach event listeners to start button, and answer buttons
+  startBtn.addEventListener("click", startGame);
